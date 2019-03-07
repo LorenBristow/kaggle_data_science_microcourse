3c895d4a69bcb1a7c189e0942c57d76b54e8dced
LorenBristow <lorendup@gmail.com>
2019-03-07 15:04:25 +0000

    validation step using mae created. very low mae since validation is being run 'in-sample'

--- machine_learning/house_price_model.py
+++ machine_learning/house_price_model.py
@@ -7,6 +7,7 @@ Created on Wed Mar  6 13:26:55 2019
 
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
 
 #STEP 1 - READ IN THE TRAINING DATA - MELBOURNE DATA#
 
@@ -49,7 +50,7 @@ melb_dataframe_missing_values_dropped = melb_dataframe.dropna(axis=0) #(axis=0) 
 #STEP 5 - SELECT PREDICTION TARGET (y) AND FEATURES (X)#
 
 #Define the prediction target 
-#We first inspect columns to identify the name of the price column in the dataset.
+#Inspect columns to identify the name of the price column in the dataset.
 print(iowa_dataframe.columns,'\n') #.columns presents the columns headers as an index. 
 #The price column in this dataset is called 'SalePrice'.
 y = iowa_dataframe.SalePrice #defines the target.
@@ -71,7 +72,17 @@ print('Model type and specifications:\n', iowa_house_price_model,'\n\n')
 
 #STEP 7 - MAKE PREDICTIONS#
 
+predicted_house_prices = iowa_house_price_model.predict(X)
 print('Predictions for the first 5 houses in the IOWA dataset are:\nFeatures of the houses:\n',X.head(), '\n',\
-      'Predicted prices of the houses:\n', iowa_house_price_model.predict(X.head()))
+      'Predicted prices of the houses:\n', iowa_house_price_model.predict(X.head()),'\n\n') #need to understand why I can't use .head with the variable 'predicted_house_prices'.
 
 #STEP 7 - MODEL VALIDATION#
+
+#'Mean absolute error' (MAE) is used as a single metric by which to assess predictove accuracy ie. model quality. 
+#The prediction error per house: error = actual price - predicted price. Take the mean of these as absolute values to get MAE. Smaller MAE is better MAE!
+print('The mean absolute error (MAE) of the model is:\n', mean_absolute_error(y,predicted_house_prices))
+
+
+
+
+
