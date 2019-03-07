50f9cd8ffafe7b4e04e6a3d30dfbae7ea411ba7c
LorenBristow <lorendup@gmail.com>
2019-03-07 13:53:16 +0000

    model defintion, fit and first predictions'

--- machine_learning/house_price_model.py
+++ machine_learning/house_price_model.py
@@ -5,10 +5,12 @@ Created on Wed Mar  6 13:26:55 2019
 @author: loren
 """
 #NOTES#
-#The model will learn and be validated based on data relating to the Melbourne housing market. 
+#The model will train and be validated based on data relating to the Melbourne housing market. 
 #The model will then be applied to data relating to the Iowa housing market ('test.csv') to make predictions for competition entry.
+#I use printing to the console for inspection of results at regular points. 
 
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 #STEP 1 - READ IN THE TRAINING DATA - MELBOURNE DATA#
 
@@ -22,6 +24,7 @@ iowa_dataframe = pd.read_csv(iowa_data_file_path)
 
 #STEP 3 - INSPECT THE DATASETS#
 
+pd.set_option('display.max_columns', None) #setting to allow all columns to be seen in console for inspection. 
 print('MELBOURNE data summary:\n\n', melb_dataframe.describe(),'\n\n')
 print('IOWA data summary:\n\n', iowa_dataframe.describe(), '\n\n')
 
@@ -57,3 +60,15 @@ print(y.head(), '\n') #to see the first few rows of 'y' for inspection.
 melb_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude'] #first version some of only the numeric variables as features.
 X = melb_dataframe_missing_values_dropped[melb_features]
 print('MELBOURNE features (X) summary:\n\n', X.describe(),'\n\n')
+
+#STEP 6 - DEFINE & FIT THE MODEL#
+
+#Using the scikit-learn library.
+#Define - The first version of the model is a decision tree model. 
+house_price_model = DecisionTreeRegressor(random_state = 1) #random state is specified to ensure the same results with each run ie. randomness is taken out. The number chose doesn't materially impact quality.
+#Fit
+house_price_model.fit(X,y)
+print('Model type and specifications:\n', house_price_model,'\n\n')
+
+#STEP 7 - MAKE PREDICTIONS#
+print('Predictions for the first 5 houses in the MELBOURNE dataset are:\nFeatures of the houses:\n',X.head(), '\n', 'Pedicted prices of the houses:\n', house_price_model.predict(X.head()))
