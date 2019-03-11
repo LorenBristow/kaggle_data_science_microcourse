42578d080d676f9257cad082ca9122a32fea9a51
LorenBristow <lorendup@gmail.com>
2019-03-11 16:17:37 +0000

    one hot encoding of categoricals appears to have no impact on MAE achieved

--- machine_learning/house_price_model.py
+++ machine_learning/house_price_model.py
@@ -11,13 +11,20 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import Imputer
+from sklearn.model_selection import cross_val_score
 
+            ##########Notes/To Dos:##########
+#show how choices were made about building the model e.g.:
+    #choosing random forest over decision tree
+    #comparing MAE for missing value handling (dropping, imputing, imputing while tracking what was mising)
+            ##########End of Notes/To Dos:##########
+            
 #Build, train and validate on Iowa data:
 iowa_data_file_path = 'input/train.csv'
 iowa_data = pd.read_csv(iowa_data_file_path) 
 y = iowa_data.SalePrice
-X = iowa_data.drop(['SalePrice'], axis=1)
-X = X.select_dtypes(exclude=['object'])
+X_with_objects = iowa_data.drop(['SalePrice'], axis=1)
+X = X_with_objects.select_dtypes(exclude=['object'])
 list_of_numerical_columns_with_missing_values = [col for col in X if X[col].isnull().any()]
 X_extn = X.copy()
 for col in list_of_numerical_columns_with_missing_values:
@@ -36,7 +43,26 @@ model = RandomForestRegressor(random_state=1)
 model_fit = model.fit(train_X_extn, train_y)
 model_predictions_training = model_fit.predict(validate_X_extn)
 model_assessment_mae = mean_absolute_error(model_predictions_training, validate_y)
-print('Mae: ', model_assessment_mae)
+print('MAE - RF, IMPUTE AND TRACKING, NO OBJECTS: ', model_assessment_mae)
+
+#Test impact of including categoricals:
+X_with_objects_ohencoded = pd.get_dummies(X_with_objects)
+list_of_object_and_num_columns_with_missing_values = [col for col in X_with_objects_ohencoded if X_with_objects_ohencoded[col].isnull().any()]
+for col in list_of_object_and_num_columns_with_missing_values:
+    X_with_objects_ohencoded[col + '_had_missing_values'] = X_with_objects_ohencoded[col].isnull()
+column_headers_X_with_objects_ohencoded = list(X_with_objects_ohencoded)
+train_X_with_obj_ohe, validate_X_with_obj_ohe, train_y, validate_y = train_test_split(X_with_objects_ohencoded, y, random_state=1)
+imputer = Imputer()
+train_X_with_obj_ohe = pd.DataFrame(imputer.fit_transform(train_X_with_obj_ohe))
+train_X_with_obj_ohe.columns = column_headers_X_with_objects_ohencoded
+validate_X_with_obj_ohe = pd.DataFrame(imputer.transform(validate_X_with_obj_ohe))
+validate_X_with_obj_ohe.columns = column_headers_X_with_objects_ohencoded
+model_ohe = RandomForestRegressor(random_state=1)
+model_fit_ohe = model_ohe.fit(train_X_with_obj_ohe, train_y)
+model_predictions_training_ohe = model_fit_ohe.predict(validate_X_with_obj_ohe)
+model_assessment_mae_ohe = mean_absolute_error(model_predictions_training_ohe, validate_y)
+print('Mae - RF, IMPUTE AND TRACKING, ONE-HOT ENCODING OBJECTS: ', model_assessment_mae)
+
 #Apply model to test data:
 test_data_file_path = 'input/test.csv'
 test_data = pd.read_csv(test_data_file_path) 
@@ -50,9 +76,12 @@ test_X_extn = pd.DataFrame(test_X_extn)
 test_X_extn.columns = column_headers_X_extn
 model_fit = model.fit(X, y)
 model_predictions_final = model_fit.predict(test_X_extn)
+
 #format for Kaggle competition submission:
-output = pd.DataFrame({'Id': test_X_extn.Id,
+output = pd.DataFrame({'Id': test_X_extn.Id.astype(int),
                        'SalePrice': model_predictions_final})
+print(output.dtypes)
+#output.Id = output.Id.astype(float)
 output.to_csv('submission.csv', index=False)
 
 
